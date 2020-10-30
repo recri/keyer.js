b02c197777115785beaee4fddc1173f94e9e9e7d
Roger E Critchlow Jr <rec@elf.org>
2020-10-30 15:37:29 -0400

    Renamed wave to ramp, corrected gain control in cancel.

--- src/KeyerPlayer.js
+++ src/KeyerPlayer.js
@@ -22,16 +22,16 @@ export class KeyerPlayer extends KeyerEvent {
     this.oscillator.start();
 
     // initialize the wave shaper
-    this.wave = this.context.createGain();
-    this.wave.gain.value = 0;
+    this.ramp = this.context.createGain();
+    this.ramp.gain.value = 0;
 
     // initialize the gain
     this.volume = this.context.createGain();
     this.gain = 0;
 
     // connect
-    this.oscillator.connect(this.wave);
-    this.wave.connect(this.volume);
+    this.oscillator.connect(this.ramp);
+    this.ramp.connect(this.volume);
   }
 
   // connect our output samples to somewhere
@@ -106,7 +106,7 @@ export class KeyerPlayer extends KeyerEvent {
   // schedule the key on at time
   keyOnAt(time) {
     // console.log("keyOnAt", time, " at ", this.context.currentTime)
-    this.wave.gain.setValueCurveAtTime(this._riseCurve, time, this.rise / 1000.0);
+    this.ramp.gain.setValueCurveAtTime(this._riseCurve, time, this.rise / 1000.0);
     this.cursor = time;
     this.emit('transition', 1, time);
   }
@@ -114,7 +114,7 @@ export class KeyerPlayer extends KeyerEvent {
   // schedule the key off at time
   keyOffAt(time) {
     // console.log("keyOffAt", time, " at ", this.context.currentTime)
-    this.wave.gain.setValueCurveAtTime(this._fallCurve, time, this.fall / 1000.0);
+    this.ramp.gain.setValueCurveAtTime(this._fallCurve, time, this.fall / 1000.0);
     this.cursor = time;
     this.emit('transition', 0, time);
   }
@@ -130,9 +130,10 @@ export class KeyerPlayer extends KeyerEvent {
   // should probably cancel all pending text and transition events, too.
   // this will cause a click if we were sounding
   cancel() {
-    // console.log("cancel at ", this.context.currentTime);
-    this.volume.gain.cancelScheduledValues((this.cursor = this.context.currentTime));
-    this.volume.gain.value = 0;
+    console.log("cancel at ", this.context.currentTime);
+    this.ramp.gain.cancelScheduledValues((this.cursor = this.context.currentTime));
+    // this.ramp.gain.setValueCurveAtTime(this._fallCurve, this.context.currentTime, this.fall / 1000.0);
+    this.ramp.gain.value = 0;
   }
 }
 // Local Variables:
