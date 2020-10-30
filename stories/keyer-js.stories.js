37317fd010fba8a266e74c78c412ecfe2b507462
Roger E Critchlow Jr <rec@elf.org>
2020-10-30 14:04:36 -0400

    Finished off the renaming to keyer-js, again.

---
+++ stories/keyer-js.stories.js
@@ -0,0 +1,11 @@
+import { html } from 'lit-element';
+import '../src/keyer-js.js';
+
+export default {
+  title: 'keyer-js',
+};
+
+export const App = () =>
+  html`
+    <keyer-js></keyer-js>
+  `;
---
+++ test/keyer-js.test.js
@@ -0,0 +1,22 @@
+import { html, fixture, expect } from '@open-wc/testing';
+
+import '../src/keyer-js.js';
+
+describe('KeyerJs', () => {
+  let element;
+  beforeEach(async () => {
+    element = await fixture(html`
+      <keyer-js></keyer-js>
+    `);
+  });
+
+  it('renders a h1', () => {
+    const h1 = element.shadowRoot.querySelector('h1');
+    expect(h1).to.exist;
+    expect(h1.textContent).to.equal('My app');
+  });
+
+  it('passes the a11y audit', async () => {
+    await expect(element).shadowDom.to.be.accessible();
+  });
+});
