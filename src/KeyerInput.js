688284ce3a598a1a966ae21d1e9e38ddf469bc6d
Roger E Critchlow Jr <rec@elf.org>
2020-11-03 20:40:22 -0500

    Made extender of KeyerPlayer, inheriting properties, passing self into Input classes.

--- src/KeyerInput.js
+++ src/KeyerInput.js
@@ -1,4 +1,5 @@
-import { KeyerEvent } from './KeyerEvent.js';
+// import { KeyerEvent } from './KeyerEvent.js';
+import { KeyerTimer } from './KeyerTimer.js';
 import { KeyerNoneInput } from './KeyerNoneInput.js';
 import { KeyerStraightInput } from './KeyerStraightInput.js';
 import { KeyerIambicInput } from './KeyerIambicInput.js';
@@ -7,7 +8,7 @@ import { KeyerMidiSource } from './KeyerMidiSource.js';
 import { KeyerKeyboardSource } from './KeyerKeyboardSource.js';
 
 // translate keyup/keydown into keyed oscillator sidetone
-export class KeyerInput extends KeyerEvent {
+export class KeyerInput extends KeyerTimer {
   constructor(context) {
     super(context);
 
@@ -16,18 +17,13 @@ export class KeyerInput extends KeyerEvent {
     this.keyboardSource = new KeyerKeyboardSource(context);
     this.midiSource.on('refresh', this.midiOnRefresh, this);
 
-    this.none = new KeyerNoneInput(context);
-    this.straight = new KeyerStraightInput(context);
-    this.iambic = new KeyerIambicInput(context);
+    this.none = new KeyerNoneInput(context, this);
+    this.straight = new KeyerStraightInput(context, this);
+    this.iambic = new KeyerIambicInput(context, this);
     this.inputs = ['none', 'straight', 'iambic'];
     this._keyer = 'none';
     this.keyer = 'none';
     this.midi = 'none';
-  }
-
-  connect(target) {
-    this.straight.connect(target);
-    this.iambic.connect(target);
   }
 
   // handlers defer to selected input type in 'iambic', 'straight', and more to come
@@ -54,43 +50,6 @@ export class KeyerInput extends KeyerEvent {
 
   get midiNotes() { return this.midiSource.notes; }
   
-  // properties of keyer
-  get pitch() { return this.iambic.pitch; }
-
-  set pitch(hertz) { this.straight.pitch = hertz; this.iambic.pitch = hertz; }
-
-  get gain() { return this.iambic.gain; }
-
-  set gain(gain) { this.iambic.gain = gain; this.straight.gain = gain; }
-
-  set weight(v) { this.iambic.weight = v; }
-
-  get weight() { return this.iambic.weight; }
-
-  set ratio(v) { this.iambic.ratio = v; }
-
-  get ratio() { return this.iambic.ratio; }
-  
-  set compensation(v) { this.iambic.compensation = v; }
-
-  get compensation() { return this.iambic.compensation; }
-  
-  get rise() { return this.iambic.rise; }
-
-  set rise(ms) { this.iambic.rise = ms; this.straight.rise = ms; }
-
-  get fall() { return this.iambic.fall; }
-
-  set fall(ms) { this.iambic.fall = ms; this.straight.fall = ms; }
-
-  get wpm() { return this.iambic.wpm; }
-
-  set wpm(wpm) { this.iambic.wpm = wpm; }
-
-  get swapped() { return this.iambic.swapped; }
-
-  set swapped(swapped) { this.iambic.swapped = swapped; }
-
   get keyer() { return this._keyer; }
 
   set keyer(keyer) {
