b45ea5ce0312bb7e1812e78fab4fc9f27b4053bb
Roger E Critchlow Jr <rec@elf.org>
2020-11-05 00:39:46 -0500

    Made event list static so different instances could share events.

--- src/KeyerEvent.js
+++ src/KeyerEvent.js
@@ -1,3 +1,4 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["on","off","emit"] }] */
 // rewrite this to drop the ctx argument to .on()
 // let everyone handle their own binding
 // add a timer, after(time, handler)
@@ -5,10 +6,40 @@
 export class KeyerEvent {
   /**
    * events: installed event handlers
+   * these became static because I had multiple
+   * chains of extends from KeyerEvent, and the
+   * events wouldn't cross between the chains.
    */
+  static events = [];
+  
+  static on(type, func, ctx) {
+    // console.log(`on ${type} ${func} ${ctx}`);
+    (KeyerEvent.events[type] = KeyerEvent.events[type] || []).push({ f: func, c: ctx });
+  }
+
+  static off(type, func) {
+    // console.log('off', type, func);
+    if (!type) KeyerEvent.events = {};
+    const list = KeyerEvent.events[type] || [];
+    let i = func ? list.length : 0;
+    // as written this loses if func occurs twice in a row
+    while (i > 0) {
+      i -= 1;
+      if (func === list[i].f) list.splice(i, 1);
+    }
+  }
+
+  static emit(type, args) {
+    const list = KeyerEvent.events[type] || [];
+    // console.log(`emit ${type} ${args} listeners ${list.length}`);
+    list.forEach(j => {
+      // console.log(`emit apply in ${j.c} ${j.f}`);
+      j.f.apply(j.c, args)
+    });
+  }
+
   constructor(audioContext) {
     this.context = audioContext;
-    this.events = [];
   }
 
   get currentTime() { return this.context.currentTime; }
@@ -20,33 +51,17 @@ export class KeyerEvent {
   /**
    *  on: listen to events
    */
-  on(type, func, ctx) {
-    // console.log('on', type, func, ctx);
-    (this.events[type] = this.events[type] || []).push({ f: func, c: ctx });
-  }
+  on(type, func, ctx) { KeyerEvent.on(type, func, ctx); }
 
   /**
    *  Off: stop listening to event / specific callback
    */
-  off(type, func) {
-    // console.log('off', type, func);
-    if (!type) this.events = {};
-    const list = this.events[type] || [];
-    let i = func ? list.length : 0;
-    // as written this loses if func occurs twice in a row
-    while (i > 0) {
-      i -= 1;
-      if (func === list[i].f) list.splice(i, 1);
-    }
-  }
+  off(type, func) { KeyerEvent.off(type, func); }
 
   /**
    * Emit: send event, callbacks will be triggered
    */
-  emit(type, ...args) {
-    const list = this.events[type] || [];
-    list.forEach(j => j.f.apply(j.c, args));
-  }
+  emit(type, ...args) { KeyerEvent.emit(type, args); }
 
   /**
    * After: fire an event at some seconds into the future.
@@ -67,13 +82,13 @@ export class KeyerEvent {
       const timer = this.context.createConstantSource();
       timer.onended = func;
       timer.start();
-      timer.stop(time);
+      timer.stop(Math.max(time,this.currentTime+1/this.sampleRate));
     }
   }
 
   // eventDebug(type) {
-  // console.log('event.debug', type, this.events[type]);
-  // console.log('event.debug', this.events);
+  // console.log('event.debug', type, this._events[type]);
+  // console.log('event.debug', this._events);
   // }
 }
 // Local Variables: 
